2dc330a02947b90d8211e534b235996a7b41568c
Fahri MacBook <fahri-can@web.de>
2019-12-29 00:26:05 +0100

    Add a method to instantiate Retrofit call.

--- app/src/main/java/com/example/swapivehicles/di/ApiModule.kt
+++ app/src/main/java/com/example/swapivehicles/di/ApiModule.kt
@@ -1,9 +1,14 @@
 package com.example.swapivehicles.di
 
 import com.example.swapivehicles.adapter.VehicleAdapter
+import com.example.swapivehicles.api_endpoint.StarWarsApi
 import com.example.swapivehicles.model.Vehicle
+import com.example.swapivehicles.service.NetworkService
 import dagger.Module
 import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 @Module
 class ApiModule {
@@ -16,5 +21,15 @@ class ApiModule {
     @Provides
     fun provideVehicleAdapter(vehicles: ArrayList<Vehicle>): VehicleAdapter {
         return VehicleAdapter(vehicles)
+    }
+
+    @Provides
+    fun provideStarWarsApi(): StarWarsApi {
+        return Retrofit.Builder()
+            .baseUrl(NetworkService.BASE_URL)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create(StarWarsApi::class.java)
     }
 }
