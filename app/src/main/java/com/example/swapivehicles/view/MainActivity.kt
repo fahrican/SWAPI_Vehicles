9dcdcb57c6a630b569257452ae9d424e30b0c0e0
Fahri MacBook <fahri-can@web.de>
2019-12-28 20:32:57 +0100

    Ensure that the class property vehicleAdapter is now instantiated.

--- app/src/main/java/com/example/swapivehicles/view/MainActivity.kt
+++ app/src/main/java/com/example/swapivehicles/view/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.swapivehicles.R
 import com.example.swapivehicles.adapter.VehicleAdapter
+import com.example.swapivehicles.di.DaggerApiComponent
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        DaggerApiComponent.create().inject(this)
     }
 }
