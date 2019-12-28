084c359839344f811249f036a6573d3c8a986f6c
Fahri MacBook <fahri-can@web.de>
2019-12-29 00:45:54 +0100

    Finish NetworkService.

--- app/src/main/java/com/example/swapivehicles/service/NetworkService.kt
+++ app/src/main/java/com/example/swapivehicles/service/NetworkService.kt
@@ -1,12 +1,23 @@
 package com.example.swapivehicles.service
 
 import com.example.swapivehicles.api_endpoint.StarWarsApi
+import com.example.swapivehicles.di.DaggerApiComponent
+import com.example.swapivehicles.model.VehiclesResult
+import io.reactivex.Single
 import javax.inject.Inject
 
 class NetworkService {
 
     @Inject
     lateinit var starWarsApi: StarWarsApi
+
+    init {
+        DaggerApiComponent.create().inject(this)
+    }
+
+    fun fetchVehicle(): Single<VehiclesResult> {
+        return starWarsApi.getVehicles()
+    }
 
     companion object {
         val BASE_URL = "https://swapi.co/"
