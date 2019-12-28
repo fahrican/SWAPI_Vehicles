9f2f5a8f81df55835cd24825b49fb34b68ab9a2d
Fahri MacBook <fahri-can@web.de>
2019-12-28 19:04:40 +0100

    Start to implement MainActivity with first injected class property.

--- app/src/main/java/com/example/swapivehicles/view/MainActivity.kt
+++ app/src/main/java/com/example/swapivehicles/view/MainActivity.kt
@@ -3,8 +3,13 @@ package com.example.swapivehicles.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.swapivehicles.R
+import com.example.swapivehicles.adapter.VehicleAdapter
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var vehicleAdapter: VehicleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
