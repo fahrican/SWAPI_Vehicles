76e648fcb1360544ebf5aa40f4e649a96fd4e2f4
Fahri MacBook <fahri-can@web.de>
2019-12-29 19:30:11 +0100

    Change type from LiveData to MutableLiveData.

--- app/src/main/java/com/example/swapivehicles/viewmodel/VehicleViewModel.kt
+++ app/src/main/java/com/example/swapivehicles/viewmodel/VehicleViewModel.kt
@@ -1,10 +1,10 @@
 package com.example.swapivehicles.viewmodel
 
-import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.swapivehicles.adapter.VehicleAdapter
+import com.example.swapivehicles.di.DaggerApiComponent
 import com.example.swapivehicles.model.Vehicle
-import com.example.swapivehicles.model.VehiclesResult
 import com.example.swapivehicles.service.NetworkService
 import io.reactivex.disposables.CompositeDisposable
 import javax.inject.Inject
@@ -18,9 +18,9 @@ class VehicleViewModel : ViewModel() {
     @Inject
     lateinit var compositeDisposable: CompositeDisposable
     @Inject
-    lateinit var vehicleListLD: LiveData<List<Vehicle>>
+    lateinit var vehicleListMLD: MutableLiveData<List<Vehicle>>
     @Inject
-    lateinit var stillInProgressLD: LiveData<Boolean>
+    lateinit var inProgressMLD: MutableLiveData<Boolean>
     @Inject
     lateinit var isErrorLD: LiveData<Boolean>
 }
