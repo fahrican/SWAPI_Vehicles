f11df23a1e3e92ee0153758aead7335e92a1cfe5
Fahri MacBook <fahri-can@web.de>
2019-12-29 02:54:28 +0100

    Add Adapter with UI logic to ViewModel.

--- app/src/main/java/com/example/swapivehicles/viewmodel/VehicleViewModel.kt
+++ app/src/main/java/com/example/swapivehicles/viewmodel/VehicleViewModel.kt
@@ -2,6 +2,7 @@ package com.example.swapivehicles.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.example.swapivehicles.adapter.VehicleAdapter
 import com.example.swapivehicles.model.Vehicle
 import com.example.swapivehicles.model.VehiclesResult
 import com.example.swapivehicles.service.NetworkService
@@ -10,6 +11,9 @@ import javax.inject.Inject
 
 class VehicleViewModel : ViewModel() {
 
+
+    @Inject
+    lateinit var vehicleAdapter: VehicleAdapter
     @Inject
     lateinit var networkService: NetworkService
     @Inject
