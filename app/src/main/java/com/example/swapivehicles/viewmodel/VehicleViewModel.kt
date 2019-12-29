4dcfb9398a4fb0fe47bb575a184c23c33c181e1c
Fahri MacBook <fahri-can@web.de>
2019-12-29 01:52:34 +0100

    Start to implement the ViewModel.

---
+++ app/src/main/java/com/example/swapivehicles/viewmodel/VehicleViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.swapivehicles.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.swapivehicles.model.Vehicle
+import com.example.swapivehicles.model.VehiclesResult
+import com.example.swapivehicles.service.NetworkService
+import io.reactivex.disposables.CompositeDisposable
+import javax.inject.Inject
+
+class VehicleViewModel : ViewModel() {
+
+    @Inject
+    lateinit var networkService: NetworkService
+    @Inject
+    lateinit var compositeDisposable: CompositeDisposable
+    @Inject
+    lateinit var vehicleListLD: LiveData<List<Vehicle>>
+    @Inject
+    lateinit var stillInProgressLD: LiveData<Boolean>
+    @Inject
+    lateinit var isErrorLD: LiveData<Boolean>
+}
