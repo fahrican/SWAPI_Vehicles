69e7e35a8a76d1645048eb997fef531b1fe26c03
Fahri MacBook <fahri-can@web.de>
2019-12-29 23:23:30 +0100

    Finish implementing MainActivity.

--- app/src/main/java/com/example/swapivehicles/view/MainActivity.kt
+++ app/src/main/java/com/example/swapivehicles/view/MainActivity.kt
@@ -2,16 +2,73 @@ package com.example.swapivehicles.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.swapivehicles.R
-import com.example.swapivehicles.adapter.VehicleAdapter
-import com.example.swapivehicles.di.DaggerApiComponent
-import javax.inject.Inject
+import com.example.swapivehicles.viewmodel.VehicleViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var vehicleViewModel: VehicleViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        vehicleViewModel = ViewModelProviders.of(this).get(VehicleViewModel::class.java)
+        vehicleViewModel.refresh()
+
+        main_swipe_refresh_layout.setOnRefreshListener {
+            main_swipe_refresh_layout.isRefreshing = false
+            vehicleViewModel.refresh()
+        }
+
+        main_recycler_view.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = vehicleViewModel.vehicleAdapter
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        observeVehicleList()
+        observeInProgress()
+        observeIsError()
+    }
+
+    private fun observeVehicleList() {
+        vehicleViewModel.vehicleListMLD.observe(this, Observer { allVehicles ->
+            allVehicles.let {
+                main_recycler_view.visibility = View.VISIBLE
+                vehicleViewModel.vehicleAdapter.setUpVehicles(it)
+            }
+        })
+    }
+
+    private fun observeInProgress() {
+        vehicleViewModel.inProgressMLD.observe(this, Observer { isLoading ->
+            isLoading.let {
+                if (it) {
+                    vehicle_fetch_progress.visibility = View.VISIBLE
+                    vehicle_fetch_error.visibility = View.GONE
+                    main_recycler_view.visibility = View.GONE
+                } else {
+                    vehicle_fetch_progress.visibility = View.GONE
+                }
+            }
+        })
+    }
+
+    private fun observeIsError() {
+        vehicleViewModel.isErrorMLD.observe(this, Observer { isError ->
+            isError.let { vehicle_fetch_error.visibility = if (it) View.VISIBLE else View.GONE }
+        })
     }
 }
