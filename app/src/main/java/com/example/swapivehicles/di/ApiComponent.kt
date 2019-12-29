3f66dd8e26d3875681e7dae21b6234e28faafcc6
Fahri MacBook <fahri-can@web.de>
2019-12-29 19:02:08 +0100

    Enable property injection in VehicleViewModel.

--- app/src/main/java/com/example/swapivehicles/di/ApiComponent.kt
+++ app/src/main/java/com/example/swapivehicles/di/ApiComponent.kt
@@ -1,13 +1,13 @@
 package com.example.swapivehicles.di
 
 import com.example.swapivehicles.service.NetworkService
-import com.example.swapivehicles.view.MainActivity
+import com.example.swapivehicles.viewmodel.VehicleViewModel
 import dagger.Component
 
 @Component(modules = [ApiModule::class])
 interface ApiComponent {
 
-    fun inject(mainActivity: MainActivity)
-
     fun inject(networkService: NetworkService)
+
+    fun inject(vehicleViewModel: VehicleViewModel)
 }
