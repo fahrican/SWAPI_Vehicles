e101f936089cc52af718b2ce33c5be2f3335217b
Fahri MacBook <fahri-can@web.de>
2019-12-29 18:58:40 +0100

    Add provide methods for properties in the ViewModel.

--- app/src/main/java/com/example/swapivehicles/di/ApiModule.kt
+++ app/src/main/java/com/example/swapivehicles/di/ApiModule.kt
@@ -1,11 +1,14 @@
 package com.example.swapivehicles.di
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.swapivehicles.adapter.VehicleAdapter
 import com.example.swapivehicles.api_endpoint.StarWarsApi
 import com.example.swapivehicles.model.Vehicle
 import com.example.swapivehicles.service.NetworkService
 import dagger.Module
 import dagger.Provides
+import io.reactivex.disposables.CompositeDisposable
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -31,5 +34,25 @@ class ApiModule {
     @Provides
     fun provideVehicleAdapter(vehicles: ArrayList<Vehicle>): VehicleAdapter {
         return VehicleAdapter(vehicles)
+    }
+
+    @Provides
+    fun provideNetworkService(): NetworkService {
+        return NetworkService()
+    }
+
+    @Provides
+    fun provideCompositeDisposable(): CompositeDisposable {
+        return CompositeDisposable()
+    }
+
+    @Provides
+    fun provideLiveDataListOfVehicles(): LiveData<List<Vehicle>> {
+        return MutableLiveData()
+    }
+
+    @Provides
+    fun provideLiveDataBoolean(): LiveData<Boolean> {
+        return MutableLiveData()
     }
 }
