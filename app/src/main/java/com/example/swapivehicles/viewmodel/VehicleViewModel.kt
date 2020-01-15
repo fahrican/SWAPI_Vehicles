15a3413a1140479cb7a5205208f285183787ce33
Fahri MacBook <fahri-can@web.de>
2020-01-15 20:27:31 +0100

    Remove adapter from ViewModel.

--- app/src/main/java/com/example/swapivehicles/viewmodel/VehicleViewModel.kt
+++ app/src/main/java/com/example/swapivehicles/viewmodel/VehicleViewModel.kt
@@ -3,7 +3,6 @@ package com.example.swapivehicles.viewmodel
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.swapivehicles.adapter.VehicleAdapter
 import com.example.swapivehicles.di.DaggerApiComponent
 import com.example.swapivehicles.model.Vehicle
 import com.example.swapivehicles.service.NetworkService
@@ -15,8 +14,6 @@ import javax.inject.Inject
 
 class VehicleViewModel : ViewModel() {
 
-    @Inject
-    lateinit var vehicleAdapter: VehicleAdapter
     @Inject
     lateinit var networkService: NetworkService
     @Inject
